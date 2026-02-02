f98ed6f67d7e2946bf83cbd39b268ec27915bc81
Seu Nome <seuemail@gmail.com>
2026-02-02 10:51:34 -0300

    comite de hoje

---
+++ android/app/src/main/java/com/example/despesas_pessoas_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.despesas_pessoas_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,8 @@ class MyHomePage extends StatelessWidget {
 
   final _transactions = [
     Transaction(id: 't1', title: 'compra sapato', value: 79.00, date: DateTime.now()),
-    Transaction(id: 't2', title: 'compra mercado', value: 132.45, date: DateTime.now())
+    Transaction(id: 't2', title: 'compra mercado', value: 132.45, date: DateTime.now()),
+    Transaction(id: 't3', title: 'pagamento de conta', value: 744, date: DateTime.now())
   ];
 
 
@@ -46,7 +47,48 @@ class MyHomePage extends StatelessWidget {
             Column(
             children: _transactions.map((tr){
               return Card(
-              child: Text(tr.title),
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                            width: 2,
+                      )
+                    ),
+                    padding: EdgeInsets.all(10),
+
+                    child: Text(
+                      tr.value.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple
+                      ),
+
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(tr.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.black
+                        ),
+                      ),
+                      Text(tr.date.toString(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.blueGrey
+                        ),),
+                    ],
+                  )
+                ],
+              )
               );
             }).toList(),
             ),
